44ebb7668fa88aee4f4e4ae771a24f109e4886d7
Alexandre Villela - SleX <slex@slex.com.br>
2015-09-17 23:17:18 -0300

    implementation tests for "capitalize_name" and "to_unicode"

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -12,8 +12,36 @@ class TestStringUtils(unittest.TestCase):
             u'Brasília'
         )
 
-    # def test_to_unicode_1(self):
-    #     self.assertEqual(
-    #         string_utils.to_unicode('BRAS\xc3\x8dLIA/PLANO PILOTO'),
-    #         u'BRASÍLIA/PLANO PILOTO'
-    #     )
+    def test_capitalize_name_2(self):
+        self.assertEqual(
+            string_utils.capitalize_name(u'BRASÍLIA/PLANO PILOTO'),
+            u'Brasília/Plano Piloto'
+        )
+
+    def test_capitalize_name_3(self):
+        self.assertEqual(
+            string_utils.capitalize_name(u'joão paulo ii'),
+            u'João Paulo II'
+        )
+
+    def test_to_unicode_str(self):
+        self.assertEqual(
+            string_utils.to_unicode('BRASILIA/PLANO PILOTO'),
+            u'BRASILIA/PLANO PILOTO'
+        )
+
+    def test_to_unicode_list(self):
+        self.assertListEqual(
+            string_utils.to_unicode(
+                ['BRASILIA/PLANO PILOTO', 1, True, 'Bolas']
+            ),
+            [u'BRASILIA/PLANO PILOTO', 1, True, u'Bolas']
+        )
+
+    def test_to_unicode_dict(self):
+        self.assertDictEqual(
+            string_utils.to_unicode(
+                dict(name='BRASILIA/PLANO PILOTO', idade=1, sport='Tenis')
+            ),
+            dict(name=u'BRASILIA/PLANO PILOTO', idade=1, sport=u'Tenis')
+        )
