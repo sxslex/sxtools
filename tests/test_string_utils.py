68921b943494cd4f1ce4e9f1d591e616b1976124
slex <alexandrebrandao.mg@diariosassociados.com.br>
2015-09-18 13:56:58 -0300

    assert test test_string_utils

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -1,4 +1,21 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    by sx.slex@gmail.com
 
 import unittest
 from sxtools import string_utils
@@ -22,6 +39,12 @@ class TestStringUtils(unittest.TestCase):
         self.assertEqual(
             string_utils.capitalize_name(u'joão paulo ii'),
             u'João Paulo II'
+        )
+
+    def test_capitalize_name_4(self):
+        self.assertEqual(
+            string_utils.capitalize_name(''),
+            u''
         )
 
     def test_to_unicode_str(self):
