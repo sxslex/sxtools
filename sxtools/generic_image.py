941f8b49cb6ad1ff93858223e1d7a89fa2b6a9e4
Alexandre Villela - SleX <slex@slex.com.br>
2015-09-19 17:56:29 -0300

    hotfix

--- sxtools/generic_image.py
+++ sxtools/generic_image.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# @ame:         generic_image.py
-# @description: Lets you create a generic image to use in development servers
-# @author:      slex
-# @create:      2015-07-06
-# @version:     0.1
-# -----------------------------------------------------------------------------
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Lets you create a generic image to use in development servers
+#    by sx.slex@gmail.com
+
 import os
 from PIL import Image
 from PIL import ImageDraw
@@ -66,19 +78,17 @@ class GenericImage(object):
         img2.seek(0)
         return img2.read()
 
-    def save(self, filename):
-        f = open(filename, 'wb')
-        f.write(self.create())
-        f.close()
-        return True
+    def save(self, filename_or_fobj):
+        if getattr(filename_or_fobj, 'read', None) is not None:
+            fobj = filename_or_fobj
+        else:
+            fobj = open(filename_or_fobj, mode='wb')
+        filename = getattr(fobj, 'name', None)
+        fobj.write(self.create())
+        fobj.close()
+        return filename
 
     def base64(self):
         import base64
         return 'data:image/' + self.iformat.lower() + ';base64,' + \
             base64.b64encode(self.create())
-
-
-# if __name__ == '__main__':
-#      gi = GenericImage(width=1, height=1, text='', iformat='JPEG')
-#      gi.save('/tmp/newimage.jpg')
-#      print 'data:image/jpeg;base64,' in gi.base64()
