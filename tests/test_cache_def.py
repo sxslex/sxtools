39d62d0f714bd66b52fd41ca7093543cb86dc074
Alexandre Villela - SleX <slex@slex.com.br>
2015-09-19 17:43:06 -0300

    assert tests

--- tests/test_cache_def.py
+++ tests/test_cache_def.py
@@ -1,41 +1,86 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    by sx.slex@gmail.com
 
-from sxtools.cache_def import cache_def
+from sxtools import cache_def
 import unittest
-
-import platform
-
+import tempfile
+import os
 foo_executando = False
+path_default = os.path.join(tempfile.gettempdir(), 'cache_def')
 
-path_default = '/tmp'
-if platform.system() == 'Windows':
-    path_default = 'c:/tmp'
-
-
-@cache_def(
-    # seed so that the cache be saved alone
-    seed='foo',
-    # directory cache
-    path=path_default,
-    # cache time in minutes
-    minuteexpire=15,
-    # debug mode
-    debug=False
-)
-def foo(a, b):
-    global foo_executando
-    foo_executando = True
-    return a + b
+try:
+    if os.path.exists(path_default):
+        os.unlink(path_default)
+except Exception:
+    pass
 
 
 class TestCacheDef(unittest.TestCase):
 
-    def test_cachedef_1(self):
+    def test_cache_def_basic(self):
+        @cache_def(
+            # seed so that the cache be saved alone
+            seed='foo_basic',
+            # directory cache
+            path=path_default,
+            # cache time in minutes
+            minuteexpire=15,
+            # debug mode
+            debug=True
+        )
+        def foo(a, b):
+            global foo_executando
+            foo_executando = True
+            return a + b
         self.assertEqual(3, foo(1, 2))
         self.assertEqual(3, foo(1, 2))
 
-    def test_cachedef_2(self):
-        global foo_executando
+    def test_cache_def_literal(self):
+        @cache_def(
+            seed='foo_literal',
+            path=path_default,
+            minuteexpire=15,
+            debug=False,
+            ftype='literal'
+        )
+        def foo(a, b):
+            global foo_executando
+            foo_executando = True
+            return a + b
+        self.assertEqual(3, foo(1, 2))
+        self.assertEqual(3, foo(1, 2))
+
+    def test_cache_def_2(self):
+        @cache_def(
+            # seed so that the cache be saved alone
+            seed='foo',
+            # directory cache
+            path=path_default,
+            # cache time in minutes
+            minuteexpire=15,
+            # debug mode
+            debug=False
+        )
+        def foo(a, b):
+            global foo_executando
+            foo_executando = True
+            return a + b
         self.assertEqual(4, foo(1, 3))
         foo_executando = False
         self.assertEqual(4, foo(1, 3))
--- tests/test_generic_image.py
+++ tests/test_generic_image.py
@@ -1,14 +1,54 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    by sx.slex@gmail.com
 
+import os
 import unittest
-from sxtools.generic_image import GenericImage
+import tempfile
+from sxtools import GenericImage
 
 
 class TestGenericImage(unittest.TestCase):
 
-    def test_geretic_image_crate(self):
+    def test_geretic_image_base64(self):
         gi = GenericImage(iformat='JPEG')
         self.assertIn(
             'data:image/jpeg;base64,',
             gi.base64()
+        )
+
+    def test_geretic_image_create_obj(self):
+        fobj = tempfile.NamedTemporaryFile(delete=False, mode='wb')
+        gi = GenericImage(iformat='JPEG')
+        filename = gi.save(fobj)
+        self.assertTrue(os.path.exists(filename))
+        os.unlink(filename)
+
+    def test_geretic_image_create_name(self):
+        filename = os.path.join(tempfile.gettempdir(), 'file_image.jpg')
+        gi = GenericImage(iformat='JPEG')
+        gi.save(filename)
+        self.assertTrue(os.path.exists(filename))
+        os.unlink(filename)
+
+    def test_geretic_image_font_not_exists(self):
+        gi = GenericImage(font_family='font_not_exists')
+        self.assertRaises(
+            Exception,
+            gi.base64
         )
